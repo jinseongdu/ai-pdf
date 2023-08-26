c86fa4ea1a01b05accc4de1d068ca674456fdf9c
jinseongdu <37732372+jinseongdu@users.noreply.github.com>
2023-08-26 19:19:24 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+# from dotenv import load_dotenv
+# load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langChain.llms import CTransformers
+
+llm=CTransformers(
+    model="llama-2-7b-chat.ggmlv3.q2_K.bin",
+    model_type="llama")
+
+
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해주세요.')
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('시 작성 중...'):
+        result = llm.predict("write a poem about:"+content + ":")
+        st.write(result)
